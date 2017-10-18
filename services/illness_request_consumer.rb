73c2db221ad59acee5d173c195d36cacc094a373
Anton <anton@ondoc.me>
2017-10-18 15:41:20 +0300

    Fix bug on logging

--- services/illness_request_consumer.rb
+++ services/illness_request_consumer.rb
@@ -6,7 +6,7 @@ class Services
     # to-do
     # нужно будет забрать IllnessRequest по id(в сообщении) из базы и обработать другими классами
     def process(message)
-      File.open("#{Dir.pwd}/hutch_logs.log", 'a') { |f| f.puts("Create IllnessRequest with id = #{message}")}
+      File.open("#{Dir.pwd}/hutch_logs.log", 'a') { |f| f.puts("Create IllnessRequest with id = #{message.body['subject']}")}
     end
   end
 end
