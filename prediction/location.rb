67abefd2f693ec63613470e8e30b84009d05e5ed
Anton <anton@ondoc.me>
2017-10-23 12:12:47 +0300

    Create clinic searsh by illness request

--- prediction/location.rb
+++ prediction/location.rb
@@ -1,17 +1,43 @@
-# require_relative '../config/application'
-require '../config/application'
-
 class Prediction
   class Location
 
-    def find_patient_location#(illness_request_id)
-      # в illness_requests по illness_request_id найти пациента и у него адрес(вернуть адрес)
-      DB[:illness_requests]
+    # return {clinic}
+    def find_nearest_clinic(illness_request_id)
+      clinics = all_clinics
+      result = {distance: distance(illness_request_id, 1), clinic: clinics.first}
+      clinics.each do |clinic|
+        current_distance = distance(illness_request_id, clinic[:id])
+        if result[:distance] > current_distance
+          result[:distance] = current_distance
+          result[:clinic] = clinic
+        end
+      end
+      result[:clinic]
+    end
+
+    # return km
+    def distance(illness_request_id, clinic_id)
+      Geocoder::Calculations.distance_between(
+          Prediction::Patient.new.find_coordinates(illness_request_id),
+          Prediction::Clinic.new.find_coordinates(clinic_id),
+          units: :km
+      )
+    end
+
+    # return [patients coordinates]
+    def find_patient_coordinates(illness_request_id)
+      Prediction::Patient.new.find_coordinates(illness_request_id)
+    end
+
+    # return [clinics coordinates]
+    def find_clinic_coordinates(clinic_id)
+      Prediction::Clinic.new.find_coordinates(clinic_id)
+    end
+
+    private
+
+    def all_clinics
+      DB[:clinics].to_a
     end
   end
-end
-
-a = Prediction::Location.new
-a.find_patient_location.each do |ir|
-  puts ir.inspect
 end
