8b1abc38f3d1b9988fa044e1d2216293b3f987b0
Anton <anton@ondoc.me>
2017-10-23 12:22:22 +0300

    Add write nearest clinic to log file

--- app.rb
+++ app.rb
@@ -1,3 +1,8 @@
+require 'geocoder'
 require_relative 'consumers/illness_request_consumer'
+require_relative 'config/application'
+require_relative 'prediction/patient'
+require_relative 'prediction/clinic'
+require_relative 'prediction/location'
 
 IllnessRequestConsumer.new
--- consumers/illness_request_consumer.rb
+++ consumers/illness_request_consumer.rb
@@ -2,9 +2,9 @@ class IllnessRequestConsumer
   include Hutch::Consumer
   consume 'illness.request.new'
 
-  # to-do
-  # нужно будет забрать IllnessRequest по id(в сообщении) из базы и обработать другими классами
   def process(message)
-    File.open("#{Dir.pwd}/hutch_logs.log", 'a') { |f| f.puts("Create IllnessRequest with id = #{message.body['subject']}")}
+    nearest_clinic = Prediction::Location.new.find_nearest_clinic(message.body['subject'])
+    report = "Create IllnessRequest with id = #{message.body['subject']}. Nearest clinic is #{nearest_clinic}"
+    File.open("#{Dir.pwd}/hutch_logs.log", 'a') { |f| f.puts(report)}
   end
 end
