bfd85f82e3458c3cc56e5e4e6229da34cb62ad9f
Anton <anton@ondoc.me>
2017-10-26 14:00:23 +0300

    Fix bug on app.rb

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 require 'geocoder'
-require_relative 'consumers/illness_request_consumer'
 require_relative 'config/application'
+require_relative 'consumers/illness_request_consumer'
 require_relative 'models/base'
 require_relative 'models/patient'
 require_relative 'models/clinic'
-require_relative 'prediction/visit_proposal'
+require_relative 'models/specialization'
 require_relative 'prediction/location'
+require_relative 'prediction/visit_proposal'
 
 IllnessRequestConsumer.new
