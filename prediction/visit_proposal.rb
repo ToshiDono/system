6cdbf31994e32d4547584f236cae07b6a8e91eca
Anton <anton@ondoc.me>
2017-10-26 13:02:46 +0300

    Refactor code

--- prediction/visit_proposal.rb
+++ prediction/visit_proposal.rb
@@ -27,39 +27,45 @@ class Prediction
     def find_doctors_in_clinics_by_specialization
       @all_specializations = find_specializations_by_symptoms if @@attempt == 1
 
-      clinic = find_nearest_clinic_by_location[:clinic]
+      @clinic = find_nearest_clinic_by_location[:clinic]
 
       unless @all_specializations.empty?
         # specialization, which are in the clinic and in the list of relevant
-        general_specializations = @all_specializations & clinic_specializations(clinic)
+        @general_specializations = @all_specializations & clinic_specializations
 
-        if !general_specializations.empty?
-          # looking for doctors in the specializations that are in the clinic
-          general_specializations.each do |spec|
-            @doctors << Clinic.new(clinic[:id]).find_doctors_by_specialization(spec[:id])
-          end
-
-          # missing specializations (which were not in the clinic)
-          @all_specializations -= general_specializations
-
-          search_repeat(clinic) unless @all_specializations.empty?
+        if !@general_specializations.empty?
+          clinic_doctors
+          missing_specializations
+          search_repeat unless @all_specializations.empty?
         else
-          search_repeat(clinic)
+          search_repeat
         end
       end
       @doctors
     end
 
     # return [{ doctors }]
-    def search_repeat(clinic)
-      @location.exclude_clinics_ids << clinic[:id]
+    def search_repeat
+      @location.exclude_clinics_ids << @clinic[:id]
       @@attempt += 1
       find_doctors_in_clinics_by_specialization
     end
 
     # return [specialization]
-    def clinic_specializations(clinic)
-      Clinic.new(clinic[:id]).specializations
+    def clinic_specializations
+      Clinic.new(@clinic[:id]).specializations
+    end
+
+    # return [specialization]
+    def missing_specializations
+      @all_specializations -= @general_specializations
+    end
+
+    # looking for doctors in the specializations that are in the clinic
+    def clinic_doctors
+      @general_specializations.each do |spec|
+        @doctors << Clinic.new(@clinic[:id]).find_doctors_by_specialization(spec[:id])
+      end
     end
   end
 end
